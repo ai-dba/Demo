3f8fdcbdd06f380c20b15f16f82bd56733a8cb31
ai-dba <144648082+ai-dba@users.noreply.github.com>
2023-11-24 00:21:00 +0330

    Uploaded on 11/23/2023 8:50:59 PM

---
+++ SQL2022/2023-06-14/User Database Files Replacement_Rollback.sql
@@ -0,0 +1,52 @@
+
+EXEC sp_configure 'show advanced options','1';
+RECONFIGURE WITH OVERRIDE;
+EXEC sp_configure 'xp_cmdshell','1';
+RECONFIGURE WITH OVERRIDE;
+EXEC xp_cmdshell 'md "C:\Program Files\Microsoft SQL Server\MSSQL16.SQL2022\MSSQL\DATA"';
+EXEC xp_cmdshell 'md "C:\Program Files\Microsoft SQL Server\MSSQL16.SQL2022\MSSQL\DATA"';
+
+CREATE TABLE #results (result nvarchar(max))
+
+
+BEGIN TRY
+	USE master
+	IF EXISTS (SELECT * FROM sys.databases WHERE name = 'AdventureWorks' AND state = 0)
+	BEGIN
+
+		IF (SELECT physical_name FROM sys.master_files WHERE database_id = DB_ID(N'AdventureWorks') AND FILE_ID = 1) = 'D:\SQL2022\SSDE_DATA\AdventureWorks.mdf'
+		BEGIN
+
+			ALTER DATABASE [AdventureWorks] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+
+			IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
+			BEGIN
+				TRUNCATE TABLE #results
+			END
+			INSERT INTO #results
+			EXEC master..xp_cmdshell 'copy "D:\SQL2022\SSDE_DATA\AdventureWorks.mdf" "C:\Program Files\Microsoft SQL Server\MSSQL16.SQL2022\MSSQL\DATA\AdventureWorks.mdf" /Z /Y';
+			IF EXISTS(SELECT * FROM #results WHERE result LIKE '%1 file(s) copied.%')
+			BEGIN
+				ALTER DATABASE [AdventureWorks] MODIFY FILE (NAME='AdventureWorks2022' , FILENAME='C:\Program Files\Microsoft SQL Server\MSSQL16.SQL2022\MSSQL\DATA\AdventureWorks.mdf');
+				ALTER DATABASE [AdventureWorks] SET ONLINE;
+				EXEC master..xp_cmdshell 'del "D:\SQL2022\SSDE_DATA\AdventureWorks.mdf" "C:\Program Files\Microsoft SQL Server\MSSQL16.SQL2022\MSSQL\DATA\AdventureWorks.mdf"';
+			END
+			ELSE
+			BEGIN
+				ALTER DATABASE [AdventureWorks] SET ONLINE;
+			END
+			TRUNCATE TABLE #results
+		END
+	END
+END TRY
+BEGIN CATCH
+	IF DB_ID(N'AdventureWorks') IS NOT NULL
+	BEGIN
+		ALTER DATABASE [AdventureWorks] SET ONLINE;
+	END
+END CATCH
+
+EXEC sp_configure 'xp_cmdshell','0';
+RECONFIGURE WITH OVERRIDE;
+EXEC sp_configure 'show advanced options','0';
+RECONFIGURE WITH OVERRIDE;
