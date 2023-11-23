01dd06736eb7641d3c0d2f1e92377d5dbfdd9cfc
ai-dba <144648082+ai-dba@users.noreply.github.com>
2023-11-24 00:23:25 +0330

    Uploaded on 11/23/2023 8:53:24 PM

---
+++ SQL2022/2023-10-27/Instance Configuration_Rollback.sql
@@ -0,0 +1,62 @@
+
+DECLARE @text nvarchar(max);
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 1;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'backup compression default', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'fill factor (%)', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+
+BEGIN TRY
+	EXEC sp_configure'optimize for ad hoc workloads', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'max server memory (MB)', 2147483647;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+
+BEGIN TRY
+	EXEC sp_configure'min server memory (MB)', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+				
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+		
