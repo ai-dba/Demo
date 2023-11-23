159c5e50477b7f287ff978440aedd73afd341bd1
ai-dba <144648082+ai-dba@users.noreply.github.com>
2023-11-24 00:21:29 +0330

    Uploaded on 11/23/2023 8:51:28 PM

---
+++ SQL2022/2023-08-25/TempDB Files Configuration_Rollback.sql
@@ -0,0 +1,3 @@
+
+ALTER DATABASE [tempdb] ADD FILE (NAME=N'temp2', FILENAME='C:\Program Files\Microsoft SQL Server\MSSQL16.SQL2022\MSSQL\DATA\tempdb_mssql_2.ndf' ,SIZE=8MB TO FILEGROUP [PRIMARY]
+			
