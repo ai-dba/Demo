51db2f29fec7bd368d93c571b934cc385b8f3c21
ai-dba <144648082+ai-dba@users.noreply.github.com>
2023-11-24 00:21:16 +0330

    Uploaded on 11/23/2023 8:51:16 PM

---
+++ SQL2022/2023-06-14/DBCC CheckDB_Change.sql
@@ -0,0 +1,36 @@
+/*** This script should be run in SQL2022 SQL Server instance. ***/
+					BEGIN TRY
+					DBCC CHECKDB ([AdventureWorks]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([master]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([model]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([msdb]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([tempdb]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
