837917fafcb5d6130490409218e819c720f7b1b0
ai-dba <144648082+ai-dba@users.noreply.github.com>
2024-02-14 09:25:33 -0800

    Uploaded on 2/14/2024 5:25:33 PM

---
+++ SQL2022/2024-02-12/DBCC CheckDB_Change.sql
@@ -0,0 +1,57 @@
+/*** This script should be run in SQL2022 SQL Server instance. ***/
+					BEGIN TRY
+					DBCC CHECKDB ([AdventureWorks]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([Advnew2022]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([AdvNewDB2022Portal]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([master]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([model]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([msdb]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([ReportDB_Copy]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([tempdb]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
