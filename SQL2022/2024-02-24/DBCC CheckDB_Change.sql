caf131d36fb3620b0f1f0c5849848d74aa3fffad
ai-dba <144648082+ai-dba@users.noreply.github.com>
2024-03-02 09:41:02 -0800

    Uploaded on 3/2/2024 5:41:02 PM

---
+++ SQL2022/2024-02-24/DBCC CheckDB_Change.sql
@@ -0,0 +1,71 @@
+/*** This script should be run in SQL2022 SQL Server instance. ***/
+					BEGIN TRY
+					DBCC CHECKDB ([AdventureWorks]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([Advnew2022]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([AdvNew2022Restored]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([AdvNew2022Restored2]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([AdvNew2022Restored3]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([AdvNewDB2022Portal]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([master]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([model]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([msdb]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
+					BEGIN TRY
+					DBCC CHECKDB ([ReportDB_Copy]) WITH NO_INFOMSGS;
+					END TRY
+					BEGIN CATCH
+					PRINT ERROR_MESSAGE()
+					END CATCH
+					
