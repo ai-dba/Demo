4bcba2bedeccc02bb593718aa0fa42fcce175182
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-01-03 05:34:24 -0800

    Uploaded on 1/3/2025 1:34:23 PM

---
+++ SQL2022/2025-01-03/Execution Plan Analysis_Change.sql
@@ -0,0 +1,8 @@
+
+/* 
+This Query has been Tuned by AI-DBA Query Optimizer (v1.7.0.0).
+Copyright (C) 2019-2025 AI-DBA Software Inc, all rights reserved.
+Date: 1/3/2025 1:29:17 PM UTC
+*/
+BEGIN TRY     CREATE NONCLUSTERED INDEX AIDBA_NCIX_250103132921 ON [ADVENTUREWORKS].[PERSON].[PASSWORD] ([ROWGUID]) ;
+ END TRY BEGIN CATCH END CATCH;
