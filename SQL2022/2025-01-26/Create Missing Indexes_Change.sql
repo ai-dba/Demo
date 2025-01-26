f262f6991823a1849b7092275045a09f361c5d6c
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-01-26 10:37:13 -0800

    Uploaded on 1/26/2025 6:37:12 PM

---
+++ SQL2022/2025-01-26/Create Missing Indexes_Change.sql
@@ -0,0 +1,416 @@
+
+/**************** CHECK *********************/
+DECLARE @pre_iid int, @pre_oid int, @pre_is_disable bit, @pre_iname nvarchar(128);
+DECLARE @pre_Smt nvarchar(max);
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF NOT EXISTS ( SELECT * FROM sys.indexes WHERE NAME = N'IX_AIDBA_1_20250126')
+	BEGIN
+		CREATE NONCLUSTERED INDEX IX_AIDBA_1_20250126
+			ON [AdventureWorks].[Person].[Person] (
+			[Title]
+			) WITH STATISTICS_ONLY ;
+
+		SELECT 
+		@pre_iid = ic.object_id , @pre_oid = ic.index_id , @pre_is_disable = i.is_disabled, @pre_iname = i.Name
+		FROM sys.indexes i
+		INNER JOIN sys.index_columns ic
+		ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+		WHERE i.name <> N'IX_AIDBA_1_20250126' AND i.object_id = OBJECT_ID('[AdventureWorks].[Person].[Person]') 
+		AND EXISTS
+		(SELECT *
+		FROM sys.indexes i2
+		INNER JOIN sys.index_columns ic2
+		ON i2.index_id = ic2.index_id AND i2.object_id = ic2.object_id 
+		WHERE i2.name = N'IX_AIDBA_1_20250126' AND i2.object_id = OBJECT_ID('[AdventureWorks].[Person].[Person]')
+		AND i.object_id = i2.object_id
+		AND i.type = i2.type
+		AND ic.index_column_id = ic2.index_column_id 
+		AND ic.column_id = ic2.column_id
+		AND ic.key_ordinal = ic2.key_ordinal
+		AND ic.partition_ordinal = ic2.partition_ordinal
+		AND ic.is_descending_key = ic2.is_descending_key
+		AND ic.is_included_column = ic2.is_included_column)
+		GROUP BY ic.object_id,ic.index_id, i.is_disabled, i.Name  
+		HAVING 
+		COUNT(*) = 1
+		AND COUNT(*) = (SELECT COUNT(*) FROM sys.index_columns ic3 WHERE ic3.index_id = ic.index_id AND ic3.object_id = ic.object_id);
+
+		IF @pre_iid IS NOT NULL
+		BEGIN
+			DROP INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person];
+			IF @pre_is_disable = 1
+			BEGIN
+				PRINT N'INDEX '+@pre_iname+' ON [AdventureWorks].[Person].[Person] WILL BE ENABLED.';
+			END
+			ELSE
+			BEGIN
+				PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] WILL NOT BE CREATED.';
+			END
+		END
+		ELSE
+		BEGIN
+			PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] WILL BE CREATED.';
+		END
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is already an index named IX_AIDBA_1_20250126 in the database.';
+	END
+ROLLBACK TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] CANNOT BE CREATED DUE TO RUNTIME ERROR.';
+END CATCH;
+
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF NOT EXISTS ( SELECT * FROM sys.indexes WHERE NAME = N'IX_AIDBA_2_20250126')
+	BEGIN
+		CREATE NONCLUSTERED INDEX IX_AIDBA_2_20250126
+			ON [AdventureWorks].[Sales].[SalesOrderHeader] (
+			[CreditCardID],[CurrencyRateID],[PurchaseOrderNumber],[ShipDate],[TerritoryID],[TotalDue]
+			) WITH STATISTICS_ONLY ;
+
+		SELECT 
+		@pre_iid = ic.object_id , @pre_oid = ic.index_id , @pre_is_disable = i.is_disabled, @pre_iname = i.Name
+		FROM sys.indexes i
+		INNER JOIN sys.index_columns ic
+		ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+		WHERE i.name <> N'IX_AIDBA_2_20250126' AND i.object_id = OBJECT_ID('[AdventureWorks].[Sales].[SalesOrderHeader]') 
+		AND EXISTS
+		(SELECT *
+		FROM sys.indexes i2
+		INNER JOIN sys.index_columns ic2
+		ON i2.index_id = ic2.index_id AND i2.object_id = ic2.object_id 
+		WHERE i2.name = N'IX_AIDBA_2_20250126' AND i2.object_id = OBJECT_ID('[AdventureWorks].[Sales].[SalesOrderHeader]')
+		AND i.object_id = i2.object_id
+		AND i.type = i2.type
+		AND ic.index_column_id = ic2.index_column_id 
+		AND ic.column_id = ic2.column_id
+		AND ic.key_ordinal = ic2.key_ordinal
+		AND ic.partition_ordinal = ic2.partition_ordinal
+		AND ic.is_descending_key = ic2.is_descending_key
+		AND ic.is_included_column = ic2.is_included_column)
+		GROUP BY ic.object_id,ic.index_id, i.is_disabled, i.Name  
+		HAVING 
+		COUNT(*) = 6
+		AND COUNT(*) = (SELECT COUNT(*) FROM sys.index_columns ic3 WHERE ic3.index_id = ic.index_id AND ic3.object_id = ic.object_id);
+
+		IF @pre_iid IS NOT NULL
+		BEGIN
+			DROP INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader];
+			IF @pre_is_disable = 1
+			BEGIN
+				PRINT N'INDEX '+@pre_iname+' ON [AdventureWorks].[Sales].[SalesOrderHeader] WILL BE ENABLED.';
+			END
+			ELSE
+			BEGIN
+				PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] WILL NOT BE CREATED.';
+			END
+		END
+		ELSE
+		BEGIN
+			PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] WILL BE CREATED.';
+		END
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is already an index named IX_AIDBA_2_20250126 in the database.';
+	END
+ROLLBACK TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] CANNOT BE CREATED DUE TO RUNTIME ERROR.';
+END CATCH;
+
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF NOT EXISTS ( SELECT * FROM sys.indexes WHERE NAME = N'IX_AIDBA_3_20250126')
+	BEGIN
+		CREATE NONCLUSTERED INDEX IX_AIDBA_3_20250126
+			ON [AdventureWorks].[Production].[WorkOrderRouting] (
+			[ActualResourceHrs],[ActualStartDate]
+			) WITH STATISTICS_ONLY ;
+
+		SELECT 
+		@pre_iid = ic.object_id , @pre_oid = ic.index_id , @pre_is_disable = i.is_disabled, @pre_iname = i.Name
+		FROM sys.indexes i
+		INNER JOIN sys.index_columns ic
+		ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+		WHERE i.name <> N'IX_AIDBA_3_20250126' AND i.object_id = OBJECT_ID('[AdventureWorks].[Production].[WorkOrderRouting]') 
+		AND EXISTS
+		(SELECT *
+		FROM sys.indexes i2
+		INNER JOIN sys.index_columns ic2
+		ON i2.index_id = ic2.index_id AND i2.object_id = ic2.object_id 
+		WHERE i2.name = N'IX_AIDBA_3_20250126' AND i2.object_id = OBJECT_ID('[AdventureWorks].[Production].[WorkOrderRouting]')
+		AND i.object_id = i2.object_id
+		AND i.type = i2.type
+		AND ic.index_column_id = ic2.index_column_id 
+		AND ic.column_id = ic2.column_id
+		AND ic.key_ordinal = ic2.key_ordinal
+		AND ic.partition_ordinal = ic2.partition_ordinal
+		AND ic.is_descending_key = ic2.is_descending_key
+		AND ic.is_included_column = ic2.is_included_column)
+		GROUP BY ic.object_id,ic.index_id, i.is_disabled, i.Name  
+		HAVING 
+		COUNT(*) = 2
+		AND COUNT(*) = (SELECT COUNT(*) FROM sys.index_columns ic3 WHERE ic3.index_id = ic.index_id AND ic3.object_id = ic.object_id);
+
+		IF @pre_iid IS NOT NULL
+		BEGIN
+			DROP INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting];
+			IF @pre_is_disable = 1
+			BEGIN
+				PRINT N'INDEX '+@pre_iname+' ON [AdventureWorks].[Production].[WorkOrderRouting] WILL BE ENABLED.';
+			END
+			ELSE
+			BEGIN
+				PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] WILL NOT BE CREATED.';
+			END
+		END
+		ELSE
+		BEGIN
+			PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] WILL BE CREATED.';
+		END
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is already an index named IX_AIDBA_3_20250126 in the database.';
+	END
+ROLLBACK TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] CANNOT BE CREATED DUE TO RUNTIME ERROR.';
+END CATCH;
+
+
+/**************** EXECUTE *********************/
+DECLARE @iid int, @oid int, @is_disable bit, @iname nvarchar(128);
+DECLARE @Smt nvarchar(max);
+
+USE [AdventureWorks];
+
+BEGIN TRY
+BEGIN TRAN
+	IF NOT EXISTS ( SELECT * FROM sys.indexes WHERE NAME = N'IX_AIDBA_1_20250126')
+	BEGIN
+		CREATE NONCLUSTERED INDEX IX_AIDBA_1_20250126
+			ON [AdventureWorks].[Person].[Person] (
+			[Title]
+			) WITH STATISTICS_ONLY ;
+
+		SELECT 
+		@iid = ic.object_id , @oid = ic.index_id , @is_disable = i.is_disabled, @iname = i.Name
+		FROM sys.indexes i
+		INNER JOIN sys.index_columns ic
+		ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+		WHERE i.name <> N'IX_AIDBA_1_20250126' AND i.object_id = OBJECT_ID('[AdventureWorks].[Person].[Person]') 
+		AND EXISTS
+		(SELECT *
+		FROM sys.indexes i2
+		INNER JOIN sys.index_columns ic2
+		ON i2.index_id = ic2.index_id AND i2.object_id = ic2.object_id 
+		WHERE i2.name = N'IX_AIDBA_1_20250126' AND i2.object_id = OBJECT_ID('[AdventureWorks].[Person].[Person]')
+		AND i.object_id = i2.object_id
+		AND i.type = i2.type
+		AND ic.index_column_id = ic2.index_column_id 
+		AND ic.column_id = ic2.column_id
+		AND ic.key_ordinal = ic2.key_ordinal
+		AND ic.partition_ordinal = ic2.partition_ordinal
+		AND ic.is_descending_key = ic2.is_descending_key
+		AND ic.is_included_column = ic2.is_included_column)
+		GROUP BY ic.object_id,ic.index_id, i.is_disabled, i.Name  
+		HAVING 
+		COUNT(*) = 1
+		AND COUNT(*) = (SELECT COUNT(*) FROM sys.index_columns ic3 WHERE ic3.index_id = ic.index_id AND ic3.object_id = ic.object_id);
+
+		IF @iid IS NOT NULL
+		BEGIN
+			DROP INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person];
+			PRINT N'HYPOTHETICAL INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] IS DROPPED DUE TO DUPLICATION.';
+			IF @is_disable = 1
+			BEGIN
+				SET @Smt = N'ALTER INDEX '+@iname+' ON [AdventureWorks].[Person].[Person] REBUILD WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);';
+				EXEC (@Smt);
+				PRINT N'INDEX '+@iname+' ON [AdventureWorks].[Person].[Person] IS ENABLED.';
+			END
+		END
+		ELSE
+		BEGIN
+			DROP INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person];
+			CREATE NONCLUSTERED INDEX IX_AIDBA_1_20250126
+			ON [AdventureWorks].[Person].[Person] (
+			[Title]
+			)WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);
+
+			PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] IS CREATED.';
+		END
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is already an index named IX_AIDBA_1_20250126 in the database.';
+	END
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] CANNOT BE CREATED.';
+END CATCH;
+
+
+USE [AdventureWorks];
+
+BEGIN TRY
+BEGIN TRAN
+	IF NOT EXISTS ( SELECT * FROM sys.indexes WHERE NAME = N'IX_AIDBA_2_20250126')
+	BEGIN
+		CREATE NONCLUSTERED INDEX IX_AIDBA_2_20250126
+			ON [AdventureWorks].[Sales].[SalesOrderHeader] (
+			[CreditCardID],[CurrencyRateID],[PurchaseOrderNumber],[ShipDate],[TerritoryID],[TotalDue]
+			) WITH STATISTICS_ONLY ;
+
+		SELECT 
+		@iid = ic.object_id , @oid = ic.index_id , @is_disable = i.is_disabled, @iname = i.Name
+		FROM sys.indexes i
+		INNER JOIN sys.index_columns ic
+		ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+		WHERE i.name <> N'IX_AIDBA_2_20250126' AND i.object_id = OBJECT_ID('[AdventureWorks].[Sales].[SalesOrderHeader]') 
+		AND EXISTS
+		(SELECT *
+		FROM sys.indexes i2
+		INNER JOIN sys.index_columns ic2
+		ON i2.index_id = ic2.index_id AND i2.object_id = ic2.object_id 
+		WHERE i2.name = N'IX_AIDBA_2_20250126' AND i2.object_id = OBJECT_ID('[AdventureWorks].[Sales].[SalesOrderHeader]')
+		AND i.object_id = i2.object_id
+		AND i.type = i2.type
+		AND ic.index_column_id = ic2.index_column_id 
+		AND ic.column_id = ic2.column_id
+		AND ic.key_ordinal = ic2.key_ordinal
+		AND ic.partition_ordinal = ic2.partition_ordinal
+		AND ic.is_descending_key = ic2.is_descending_key
+		AND ic.is_included_column = ic2.is_included_column)
+		GROUP BY ic.object_id,ic.index_id, i.is_disabled, i.Name  
+		HAVING 
+		COUNT(*) = 6
+		AND COUNT(*) = (SELECT COUNT(*) FROM sys.index_columns ic3 WHERE ic3.index_id = ic.index_id AND ic3.object_id = ic.object_id);
+
+		IF @iid IS NOT NULL
+		BEGIN
+			DROP INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader];
+			PRINT N'HYPOTHETICAL INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] IS DROPPED DUE TO DUPLICATION.';
+			IF @is_disable = 1
+			BEGIN
+				SET @Smt = N'ALTER INDEX '+@iname+' ON [AdventureWorks].[Sales].[SalesOrderHeader] REBUILD WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);';
+				EXEC (@Smt);
+				PRINT N'INDEX '+@iname+' ON [AdventureWorks].[Sales].[SalesOrderHeader] IS ENABLED.';
+			END
+		END
+		ELSE
+		BEGIN
+			DROP INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader];
+			CREATE NONCLUSTERED INDEX IX_AIDBA_2_20250126
+			ON [AdventureWorks].[Sales].[SalesOrderHeader] (
+			[CreditCardID],[CurrencyRateID],[PurchaseOrderNumber],[ShipDate],[TerritoryID],[TotalDue]
+			)WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);
+
+			PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] IS CREATED.';
+		END
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is already an index named IX_AIDBA_2_20250126 in the database.';
+	END
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] CANNOT BE CREATED.';
+END CATCH;
+
+
+USE [AdventureWorks];
+
+BEGIN TRY
+BEGIN TRAN
+	IF NOT EXISTS ( SELECT * FROM sys.indexes WHERE NAME = N'IX_AIDBA_3_20250126')
+	BEGIN
+		CREATE NONCLUSTERED INDEX IX_AIDBA_3_20250126
+			ON [AdventureWorks].[Production].[WorkOrderRouting] (
+			[ActualResourceHrs],[ActualStartDate]
+			) WITH STATISTICS_ONLY ;
+
+		SELECT 
+		@iid = ic.object_id , @oid = ic.index_id , @is_disable = i.is_disabled, @iname = i.Name
+		FROM sys.indexes i
+		INNER JOIN sys.index_columns ic
+		ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+		WHERE i.name <> N'IX_AIDBA_3_20250126' AND i.object_id = OBJECT_ID('[AdventureWorks].[Production].[WorkOrderRouting]') 
+		AND EXISTS
+		(SELECT *
+		FROM sys.indexes i2
+		INNER JOIN sys.index_columns ic2
+		ON i2.index_id = ic2.index_id AND i2.object_id = ic2.object_id 
+		WHERE i2.name = N'IX_AIDBA_3_20250126' AND i2.object_id = OBJECT_ID('[AdventureWorks].[Production].[WorkOrderRouting]')
+		AND i.object_id = i2.object_id
+		AND i.type = i2.type
+		AND ic.index_column_id = ic2.index_column_id 
+		AND ic.column_id = ic2.column_id
+		AND ic.key_ordinal = ic2.key_ordinal
+		AND ic.partition_ordinal = ic2.partition_ordinal
+		AND ic.is_descending_key = ic2.is_descending_key
+		AND ic.is_included_column = ic2.is_included_column)
+		GROUP BY ic.object_id,ic.index_id, i.is_disabled, i.Name  
+		HAVING 
+		COUNT(*) = 2
+		AND COUNT(*) = (SELECT COUNT(*) FROM sys.index_columns ic3 WHERE ic3.index_id = ic.index_id AND ic3.object_id = ic.object_id);
+
+		IF @iid IS NOT NULL
+		BEGIN
+			DROP INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting];
+			PRINT N'HYPOTHETICAL INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] IS DROPPED DUE TO DUPLICATION.';
+			IF @is_disable = 1
+			BEGIN
+				SET @Smt = N'ALTER INDEX '+@iname+' ON [AdventureWorks].[Production].[WorkOrderRouting] REBUILD WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);';
+				EXEC (@Smt);
+				PRINT N'INDEX '+@iname+' ON [AdventureWorks].[Production].[WorkOrderRouting] IS ENABLED.';
+			END
+		END
+		ELSE
+		BEGIN
+			DROP INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting];
+			CREATE NONCLUSTERED INDEX IX_AIDBA_3_20250126
+			ON [AdventureWorks].[Production].[WorkOrderRouting] (
+			[ActualResourceHrs],[ActualStartDate]
+			)WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);
+
+			PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] IS CREATED.';
+		END
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is already an index named IX_AIDBA_3_20250126 in the database.';
+	END
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] CANNOT BE CREATED.';
+END CATCH;
+
