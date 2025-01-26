b2e4e16b9ac2fdc7ddee0de57153f507a8d1fba2
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-01-26 10:37:11 -0800

    Uploaded on 1/26/2025 6:37:11 PM

---
+++ SQL2022/2025-01-26/Create Missing Indexes_Rollback.sql
@@ -0,0 +1,63 @@
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_1_20250126')
+	BEGIN
+		DROP INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person];
+		PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_1_20250126 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_1_20250126 ON [AdventureWorks].[Person].[Person] CANNOT BE DROPPED.';
+END CATCH;
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_2_20250126')
+	BEGIN
+		DROP INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader];
+		PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_2_20250126 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_2_20250126 ON [AdventureWorks].[Sales].[SalesOrderHeader] CANNOT BE DROPPED.';
+END CATCH;
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_3_20250126')
+	BEGIN
+		DROP INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting];
+		PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_3_20250126 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_3_20250126 ON [AdventureWorks].[Production].[WorkOrderRouting] CANNOT BE DROPPED.';
+END CATCH;
