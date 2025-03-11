cfb0f1986f73b6966a6b6baeb847c788e6892a66
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-03-11 00:14:11 -0700

    Uploaded on 3/11/2025 7:14:11 AM

---
+++ SQL2022/2025-03-11/Instance Configuration_Rollback.sql
@@ -0,0 +1,46 @@
+
+DECLARE @text nvarchar(max);
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 1;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'max server memory (MB)', 24693;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+
+BEGIN TRY
+	EXEC sp_configure'min server memory (MB)', 3086;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+				
+BEGIN TRY
+	EXEC sp_configure'cost threshold for parallelism', 5;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+				
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+		
