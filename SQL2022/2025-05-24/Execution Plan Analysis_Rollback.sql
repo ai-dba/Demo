652c6d2baef399565a832a62843839ddd81ec34e
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-05-23 18:12:14 -0700

    Uploaded on 5/24/2025 1:12:13 AM

---
+++ SQL2022/2025-05-24/Execution Plan Analysis_Rollback.sql
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+BEGIN TRY 
+    DROP INDEX [IX_AIDBA_2_20250506] ON [ADVENTUREWORKS].[SALES].[SALESORDERDETAIL]; 
+END TRY 
+BEGIN CATCH 
+END CATCH;
