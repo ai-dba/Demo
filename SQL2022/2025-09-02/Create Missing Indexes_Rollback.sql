b27c5548160a3261f6dcef4e62c0d1eb57380973
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-09-01 21:57:55 -0700

    Uploaded on 9/2/2025 4:57:55 AM

---
+++ SQL2022/2025-09-02/Create Missing Indexes_Rollback.sql
@@ -0,0 +1,21 @@
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_1_20250902')
+	BEGIN
+		DROP INDEX IX_AIDBA_1_20250902 ON [AdventureWorks].[Sales].[SalesOrderHeader];
+		PRINT N'INDEX IX_AIDBA_1_20250902 ON [AdventureWorks].[Sales].[SalesOrderHeader] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_1_20250902 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_1_20250902 ON [AdventureWorks].[Sales].[SalesOrderHeader] CANNOT BE DROPPED.';
+END CATCH;
