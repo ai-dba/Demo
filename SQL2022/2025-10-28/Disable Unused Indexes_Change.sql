e3acfdab82aa3e46f60c0133d4bd1548cc0e0760
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-11-05 23:02:58 -0800

    Uploaded on 11/6/2025 7:02:57 AM

---
+++ SQL2022/2025-10-28/Disable Unused Indexes_Change.sql
@@ -0,0 +1,13 @@
+
+USE [AdventureWorks]
+BEGIN TRY
+	IF EXISTS(  SELECT 1 FROM SYS.INDEXES I WHERE (I.[IS_PRIMARY_KEY] = 0 AND I.[IS_UNIQUE] = 0) AND I.[NAME] = 'IX_Orderqty_ProductID_UnitPrice-20250929-040322')
+	BEGIN
+	ALTER INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322]
+	ON [AdventureWorks].[Sales].[SalesOrderDetail] DISABLE;
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks].[Sales].[SalesOrderDetail] IS DISABLED.';
+	END
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks].[Sales].[SalesOrderDetail] CANNOT BE DISABLED.';
+END CATCH;
