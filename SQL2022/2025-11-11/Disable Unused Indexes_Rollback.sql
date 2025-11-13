0a84ba287ac106820ba712c7b724ddbbc1116b4f
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-11-12 21:20:13 -0800

    Uploaded on 11/13/2025 5:20:12 AM

---
+++ SQL2022/2025-11-11/Disable Unused Indexes_Rollback.sql
@@ -0,0 +1,10 @@
+
+USE [AdventureWorks]
+BEGIN TRY
+	ALTER INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322]
+	ON [AdventureWorks].[Sales].[SalesOrderDetail] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks].[Sales].[SalesOrderDetail] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks].[Sales].[SalesOrderDetail] CANNOT BE ENABLED.';
+END CATCH;
