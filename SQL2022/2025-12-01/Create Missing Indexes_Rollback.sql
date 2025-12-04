88a2085e59ac9fd9604d4f88e7de77b3dfe06bcd
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-12-03 16:39:36 -0800

    Uploaded on 12/4/2025 12:39:36 AM

---
+++ SQL2022/2025-12-01/Create Missing Indexes_Rollback.sql
@@ -0,0 +1,84 @@
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_1_20251201')
+	BEGIN
+		DROP INDEX IX_AIDBA_1_20251201 ON [AdventureWorks].[Sales].[SalesOrderHeader];
+		PRINT N'INDEX IX_AIDBA_1_20251201 ON [AdventureWorks].[Sales].[SalesOrderHeader] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_1_20251201 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_1_20251201 ON [AdventureWorks].[Sales].[SalesOrderHeader] CANNOT BE DROPPED.';
+END CATCH;
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_2_20251201')
+	BEGIN
+		DROP INDEX IX_AIDBA_2_20251201 ON [AdventureWorks].[Person].[Person];
+		PRINT N'INDEX IX_AIDBA_2_20251201 ON [AdventureWorks].[Person].[Person] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_2_20251201 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_2_20251201 ON [AdventureWorks].[Person].[Person] CANNOT BE DROPPED.';
+END CATCH;
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_3_20251201')
+	BEGIN
+		DROP INDEX IX_AIDBA_3_20251201 ON [AdventureWorks].[Production].[WorkOrderRouting];
+		PRINT N'INDEX IX_AIDBA_3_20251201 ON [AdventureWorks].[Production].[WorkOrderRouting] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_3_20251201 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_3_20251201 ON [AdventureWorks].[Production].[WorkOrderRouting] CANNOT BE DROPPED.';
+END CATCH;
+
+USE [AdventureWorks];
+BEGIN TRY
+BEGIN TRAN
+	IF EXISTS ( SELECT * FROM sys.indexes WHERE name = N'IX_AIDBA_4_20251201')
+	BEGIN
+		DROP INDEX IX_AIDBA_4_20251201 ON [AdventureWorks].[Production].[WorkOrder];
+		PRINT N'INDEX IX_AIDBA_4_20251201 ON [AdventureWorks].[Production].[WorkOrder] IS DROPPED.';
+	END
+	ELSE
+	BEGIN
+		PRINT N'There is no index named IX_AIDBA_4_20251201 in the database.';
+	END
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+ROLLBACK TRAN
+	PRINT N'INDEX IX_AIDBA_4_20251201 ON [AdventureWorks].[Production].[WorkOrder] CANNOT BE DROPPED.';
+END CATCH;
