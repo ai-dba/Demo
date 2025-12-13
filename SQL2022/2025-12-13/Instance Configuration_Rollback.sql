5f92b9187635903745f9bfde778685d244b434b7
ai-dba <144648082+ai-dba@users.noreply.github.com>
2025-12-12 21:34:45 -0800

    Uploaded on 12/13/2025 5:34:44 AM

---
+++ SQL2022/2025-12-13/Instance Configuration_Rollback.sql
@@ -0,0 +1,37 @@
+
+DECLARE @text nvarchar(max);
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 1;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'fill factor (%)', 90;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+
+BEGIN TRY
+	EXEC sp_configure'cost threshold for parallelism', 1;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+				
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+		
