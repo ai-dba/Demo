250cc6ee29dad1506db244aa78d255ab4dd98e12
ai-dba <144648082+ai-dba@users.noreply.github.com>
2026-01-11 16:54:54 -0800

    Uploaded on 1/12/2026 12:54:54 AM

---
+++ SQL2022/2026-01-12/Rename sa Login_Change.sql
@@ -0,0 +1,8 @@
+
+				/*** Login "sa" has been changed to "System_Administrator_20260112", and new fake login "SA" has been created with password = "P@s$0.176375w0Rd". ***/
+
+				USE [master];
+				ALTER LOGIN [sa] WITH NAME = System_Administrator_20260112;
+				CREATE LOGIN SA WITH PASSWORD=N'P@s$0.176375w0Rd', DEFAULT_DATABASE=[tempdb], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF;
+
+			
