74c94142f19ee8ece6d311bf099171302d200beb
AI-DBA for SQL Server <144648082+ai-dba@users.noreply.github.com>
2026-01-31 03:34:37 -0800

    Uploaded on 1/31/2026 11:34:37 AM

---
+++ SQL2022/2026-01-31/Disable Unused Indexes_Rollback.sql
@@ -0,0 +1,30 @@
+
+USE [AdventureWorks1]
+BEGIN TRY
+	ALTER INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322]
+	ON [AdventureWorks1].[Sales].[SalesOrderDetail] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks1].[Sales].[SalesOrderDetail] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks1].[Sales].[SalesOrderDetail] CANNOT BE ENABLED.';
+END CATCH;
+
+USE [adventureworks2]
+BEGIN TRY
+	ALTER INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322]
+	ON [adventureworks2].[Sales].[SalesOrderDetail] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [adventureworks2].[Sales].[SalesOrderDetail] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [adventureworks2].[Sales].[SalesOrderDetail] CANNOT BE ENABLED.';
+END CATCH;
+
+USE [AdventureWorks]
+BEGIN TRY
+	ALTER INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322]
+	ON [AdventureWorks].[Sales].[SalesOrderDetail] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks].[Sales].[SalesOrderDetail] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_Orderqty_ProductID_UnitPrice-20250929-040322] ON [AdventureWorks].[Sales].[SalesOrderDetail] CANNOT BE ENABLED.';
+END CATCH;
